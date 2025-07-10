213c39f85a02d68870480e0bf4e5d7f3f7480c1a
Tane <94614445+Tane-N@users.noreply.github.com>
2025-07-10 21:36:58 +0300

    pointer event handler

---
+++ Assets/Scripts/PointerEventHandler.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class PointerEventHandler : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public UnityEvent onPointerClick;
+    public UnityEvent onPointerDown;
+    public UnityEvent onPointerUp;
+    public UnityEvent onPointerEnter;
+    public UnityEvent onPointerExit;
+
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        onPointerClick?.Invoke();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        onPointerDown?.Invoke();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        onPointerUp?.Invoke();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        onPointerEnter?.Invoke();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        onPointerExit?.Invoke();
+    }
+
+    public void DebugMessage(string message)
+    {
+        Debug.Log(message);
+    }
+}
